7f8300d04f1f4391a30cc168c5898e54cf6a3f04
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-03-19 11:41:35 +0100

    Refined the overview

--- MazeBook.playgroundbook/Contents/Chapters/Document1.playgroundchapter/Pages/Exercise1.playgroundpage/Contents.swift
+++ MazeBook.playgroundbook/Contents/Chapters/Document1.playgroundchapter/Pages/Exercise1.playgroundpage/Contents.swift
@@ -7,26 +7,22 @@
 //#-end-hidden-code
 /*:
 
- **About pathfinding algorithms**
+ **Pathfinding algorithms**
 
- A pathfinding algorithm describes a method that searches a graph by starting at a given point and looking at all neighboring points until the destination is reached.
+ A pathfinding algorithm is a method that can find a path between two points in a graph.
 
  There are different pathfinding methods:
  - [Breadth-first search](glossary://Breadth-first%20search) works by starting at the root and exploring all neighbor nodes first, before moving to the next level neighbors
  - [Dijkstra's algorithm](glossary://Dijkstra's%20algorithm) which, given a node will find the path between that node and all other nodes, therefore finding the shortest path between two nodes
- - A* is an extension of Dijkstra's algorithm that uses heuristics to improve performance ans is widely used in games **TODO** where else used?
+ - A* is an extension of Dijkstra's algorithm that uses heuristics to improve performance and is widely used in games
 
  **About this Playground**
 
- The GameplayKit framework already includes a pathfinding API, which uses a version of the A* algorithm.
+ Tap two tiles in the maze to calculate a path between them. Tap the _Run 100x_ button to gather a larger sample size.
 
- In this initial example, you can select two tiles on the maze to gather some sample data on the performance of the included A* algorithm.
-
- Tap the _Run 100x_ button to gather a larger sample size.
- As you can see, the A* implementation in GameplayKit is incredibly fast, finding a path through the maze usually takes less than a millisecond, depending on the complexity of the maze.
+ This introduction page uses a pathfinding algorithm provided the GameplayKit framework.
 
  In the next chapter, you'll be tasked with writing your own pathfinding algorithm. The final chapter contains an example implementation of the A* pathfinding algorithm.
-
 
  */
 //#-hidden-code
