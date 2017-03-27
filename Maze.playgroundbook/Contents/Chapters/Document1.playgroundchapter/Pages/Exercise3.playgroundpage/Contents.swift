ca6ab8d05a650e6c3e09430cad7a278f0763a43d
Lukas Kollmer <lukas.kollmer@gmail.com>
2017-03-27 07:19:49 +0200

    A*-Exercise: Added a little introduction, removed old example code

--- Maze.playgroundbook/Contents/Chapters/Document1.playgroundchapter/Pages/Exercise3.playgroundpage/Contents.swift
+++ Maze.playgroundbook/Contents/Chapters/Document1.playgroundchapter/Pages/Exercise3.playgroundpage/Contents.swift
@@ -10,6 +10,10 @@
 /*:
  **Goal:** In this exercise, you'll write code that gives the path between two tiles using the A* algorithm.
 
+ Your A*-Algorithm should look at each tile in the maze and assign a value to it, it's so called weight.
+ A Tile's weight is equal to the distance between that tile and the initial tile, plus the distance between that tile and the destination tile.
+
+ When you reach the destination Tile, you can just walk back
 
  */
 //#-hidden-code
@@ -31,71 +35,8 @@ func solveMaze(_ algorithm: GameLogic.Algorithm) {
 
 //#-editable-code
 
-struct PathStep: Hashable {
-    let location: TileLocation
-    var previous: Indirect<PathStep>?
-
-    init(location: TileLocation) {
-        self.location = location
-    }
-
-    var hashValue: Int {
-        return self.location.hashValue
-    }
-
-    var path: [TileLocation] {
-        var path = [TileLocation]()
-        var currentStep = self
-        while let parent = currentStep.previous {
-            path.insert(currentStep.location, at: 0)
-            currentStep = parent.value
-        }
-        return path
-    }
-}
-
-func ==(lhs: PathStep, rhs: PathStep) -> Bool {
-    return lhs.location == rhs.location
-}
-
 func findPath(startTile: Tile, endTile: Tile) -> [TileLocation] {
     guard let maze = startTile.mazeScene else { return [] }
-
-    var queue = Array<PathStep>()
-    queue.append(PathStep(location: startTile.location))
-
-    var exploredSteps = Set<PathStep>()
-
-    while !queue.isEmpty {
-        let currentStep = queue.removeFirst()
-        exploredSteps.insert(currentStep)
-
-        if currentStep.location == endTile.location {
-            return currentStep.path
-        }
-
-        for location in maze.tile(atLocation: currentStep.location).neighboringTiles {
-            guard maze.tile(atLocation: location).state == .path else { continue }
-
-            var step = PathStep(location: location)
-
-            if exploredSteps.contains(step) {
-                continue
-            }
-
-            if let existingIndex = queue.index(of: step) {
-                var step = queue[existingIndex]
-                step.previous = Indirect<PathStep>(currentStep)
-
-                queue.remove(at: existingIndex)
-                queue.append(step)
-            } else {
-                step.previous = Indirect<PathStep>(currentStep)
-                queue.append(step)
-            }
-
-        }
-    }
 
     return []
 }
